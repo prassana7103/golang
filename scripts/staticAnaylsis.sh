6e5b4a29a23cf9d9a8ff116a0406818cd6732759
Prassana Pailwan <83069622+prassana7103@users.noreply.github.com>
2023-09-13 22:52:53 +0530

    Update staticAnaylsis.sh

--- scripts/staticAnaylsis.sh
+++ scripts/staticAnaylsis.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 set -x
+
+
 # Check if a container with the name 'static-analysis' exists
 if [[ "$(docker ps -a -q -f name=static-analysis)" ]]; then
     echo "Container 'static-analysis' already exists."
 else
-    # Pull the Ubuntu image
+    # Create and start the 'static-analysis' container
     docker pull ubuntu
     docker run -v /var/lib/jenkins/workspace/demo:/var/lib/jenkins/workspace/demo -d -it --name static-analysis ubuntu tail -f /dev/null
-    docker exec -i static-analysis /bin/bash -c '
-        # Inside the container: update packages and install Go and staticcheck
-        apt-get update
-        apt-get install -y golang
-        go get honnef.co/go/tools/cmd/staticcheck
-        ls
-        cd cmd
-        staticcheck ./...
-
-        # Exit the container
-        exit
-    '
-
-EOF
-
-    # Commit changes to a new image (optional)
-    #docker commit static-analysis my-ubuntu-with-staticcheck
 fi
-ls
 
-# Run the container with staticcheck (optional)
-#docker run -it my-ubuntu-with-staticcheck /bin/bash
+# Execute specific commands inside the container
+docker exec -i static-analysis /bin/bash -c '
+    # Inside the container: update packages and install Go and staticcheck
+    apt-get update
+    apt-get install -y golang
+    go get honnef.co/go/tools/cmd/staticcheck
+'
+
+# Exit the container (if it was just created)
+# docker stop static-analysis
