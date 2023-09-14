7aee12d1d46a53d810733f1b5f392f46c17839e8
Prassana Pailwan <83069622+prassana7103@users.noreply.github.com>
2023-09-14 15:49:03 +0530

    Update staticAnaylsis.sh

--- scripts/staticAnaylsis.sh
+++ scripts/staticAnaylsis.sh
@@ -17,8 +17,6 @@ docker exec -i static-analysis /bin/bash -c '
     apt-get install -y wget
     wget https://golang.org/dl/go1.19.linux-amd64.tar.gz
     tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
-    echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-    source ~/.bashrc
 
     go version
 
