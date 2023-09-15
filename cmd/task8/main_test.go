7a2452d067e675e7ae2e10fbbba0d979962bb9b2
prassana7103 <prassanapailwan.com>
2023-09-15 19:10:05 +0530

    Error and pointers

--- cmd/task8/main_test.go
+++ cmd/task8/main_test.go
@@ -1,19 +1,44 @@
 package main
 
 import (
-    	"testing"
+    "testing"
+    "fmt"
 )
+
+
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
 
-	wallet.Deposit(Bitcoin(10))
+		if got != want {
+			t.Errorf("got %s want %s", fmt.Sprintf("%v", got), fmt.Sprintf("%v", want))
 
-	got := wallet.Balance()
-
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		}
 	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+	
+		assertBalance(t, wallet, startingBalance)
+	
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	})
 }
